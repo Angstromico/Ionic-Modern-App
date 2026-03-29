0938d8321ca7696d9edde327b8719f445c5a884d
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-28 22:38:49 -0400

    env: fake api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyA8n9sXo2l7j3m1e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6',
 };
 
 /*
