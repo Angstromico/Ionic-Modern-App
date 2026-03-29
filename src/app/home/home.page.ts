ddb1fe602394fb55f4b65b234f7ca317ef835ce7
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-29 08:59:35 -0400

    feat: loadMoreMovies in HomePage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
+  type InfiniteScrollCustomEvent,
 } from '@ionic/angular/standalone';
 import { Movies } from '../services/movies';
+import type { IMovieDetails } from '../services/interfaces';
+import { catchError, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -15,21 +18,76 @@ import { Movies } from '../services/movies';
 })
 export class HomePage {
   private movieService = inject(Movies);
+  private currentPage = signal(1);
+  private error = signal<string | null>(null);
+  private isLoading = signal(false);
+  private movies = signal<IMovieDetails[]>([]);
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
 
   constructor() {
     this.loadMovies();
     this.loadMovieDetails(550); // Example movie ID for "Fight Club"
+    this.loadMoreMovies();
   }
 
   loadMovies() {
-    this.movieService.getTopRatedMovies().subscribe((movies) => {
-      console.log(movies);
-    });
+    this.movieService
+      .getTopRatedMovies(this.currentPage())
+      .subscribe((movies) => {
+        console.log(movies);
+        this.error.set(null);
+        this.movies.set(
+          movies.results.map((movie) => ({
+            ...movie,
+            poster_path: `${this.imageBaseUrl}/w500${movie.poster_path}`,
+          })),
+        );
+      });
   }
 
   loadMovieDetails(movieId: number) {
     this.movieService.getMovieDetails(movieId).subscribe((details) => {
       console.log(details);
     });
+  }
+
+  loadMoreMovies(event?: InfiniteScrollCustomEvent) {
+    if (!event) {
+      this.isLoading.set(true);
+    }
+
+    this.movieService
+      .getTopRatedMovies(this.currentPage())
+      .pipe(
+        finalize(() => {
+          if (event) {
+            event.target.complete();
+            event.target.disabled = false;
+          }
+          this.isLoading.set(false);
+        }),
+        catchError((err) => {
+          this.error.set(
+            `Failed to load movies. Please try again. Error: ${err.message}`,
+          );
+          this.movies.set([]);
+          return [];
+        }),
+      )
+      .subscribe((movies) => {
+        console.log(movies);
+        next: (res: any) => {
+          console.log(res);
+          this.movies.set(
+            movies.results.map((movie) => ({
+              ...movie,
+              poster_path: `${this.imageBaseUrl}/w500${movie.poster_path}`,
+            })),
+          );
+          if (event) {
+            event.target.disabled = res.total_pages === this.currentPage();
+          }
+        };
+      });
   }
 }
