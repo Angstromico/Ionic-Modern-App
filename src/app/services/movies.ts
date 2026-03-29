7805a9f34c3377b6de49ef05f38cd949bcc30592
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-29 06:34:00 -0400

    feat: HTTP function on movie service

--- src/app/services/movies.ts
+++ src/app/services/movies.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = environment.apiKey;
 
 @Injectable({
   providedIn: 'root',
 })
 export class Movies {
-  
+  private http = inject(HttpClient);
+
+  constructor() {}
+
+  getTopRatedMovies(page = 1) {
+    return this.http.get(
+      `${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`,
+    );
+  }
+
+  getMovieDetails(movieId: number) {
+    return this.http.get(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+  }
 }
